23ef566a2f24424e95e1319296855df0c87240fa
Nightgust <nightgust@gmail.com>
2020-12-13 14:45:00 +0300

    переаботал модуль загрузки товаров - убрал разделение на варианты загрузки и опитимизировал название функций и переменных

--- classification.py
+++ classification.py
@@ -2,57 +2,45 @@ from sklearn.naive_bayes import BernoulliNB  # импорт НБ Бернули
 from sklearn.feature_extraction.text import CountVectorizer  # импортируем класс преобразования из текста в вектор
 import numpy as np
 from load_file_test import FileLoader
-
-path = 'teach_file_sample.xlsx'  # путь к загружаемому файлу
-path2='teach_test_sample.xlsx'
-file_beforehand=FileLoader.load_file_new(path,1)
-
-# загружаем обучающую выборку: описание товаров
-model_goods_description = file_beforehand[0]
-#model_goods_description1 = ['Женщины верхняя одежда куртка джинсовая куртка', 'Мужчины верхняя одежда куртка пуховик']
-
-# загружаем обучающую выборку: значения категорий для обучающего множества товаров
-model_goods_class_values = np.array(file_beforehand[1])
-#model_goods_class_values = np.array(FileLoader.load_file_new(path,1)[1])
-
-#model_goods_class_values1 = np.array([1, 2])
-
-# создаем пустой словарь
-model_vocabular = CountVectorizer()
-#model_vocabular1 = CountVectorizer()
-
-# заполняем словарь и токенизируем
-model_vocabular.fit(model_goods_description)
-#print(model_vocabular.vocabulary_)
-#model_vocabular1.fit(model_goods_description1)
-
-# создаем набор векторов обучающего множества товаров
-model_goods_vectors = model_vocabular.transform(model_goods_description)
-#print(model_goods_vectors.shape)
-#model_goods_vectors1 = model_vocabular.transform(model_goods_description1)
-
-# создаем пустую модель
-model = BernoulliNB()
-#model1 = BernoulliNB()
-
-# обучаем модель
-model.fit(model_goods_vectors.toarray(), model_goods_class_values)
-#model1.fit(model_goods_vectors1.toarray(), model_goods_class_values1)
-
-# загружаем описание товара для классификации
-good_without_class_description = FileLoader.load_file_new(path2,2)[0]
-print(good_without_class_description)
-
-# перводим описание товара в векторную форму используя созданный ранее словарб
-good_without_class_vector = model_vocabular.transform(good_without_class_description)
-
-# получаем предполагаемую категорию в классификаторе
-good_class = model.predict(good_without_class_vector.toarray())
-print('Рузультат классификации:',good_class, '\n', file_beforehand[2])
-
-#статистика по тестированию
+from statistic import quality_classification
 
 
+def teach_classifier(path):
+    file_beforehand = FileLoader.load_file(path) # проводим предварительные преобразование обучащей выборки
+    model_goods_description = file_beforehand[0]  # список с описанием товаров для обучения
+    model_goods_class_values = np.array(file_beforehand[1])  # список со значениями категорий для обучения
+    model_vocabular = CountVectorizer()  # инициируем пустой словарь
+
+    model_vocabular.fit(model_goods_description)  # заполняем словарь и токенизируем
+    # print(model_vocabular.vocabulary_)
+    model_goods_vectors = model_vocabular.transform(
+        model_goods_description)  # создаем набор векторов обучающего множества товаров
+    # print(model_goods_vectors.shape)
+    model = BernoulliNB()  # создаем пустую модель
+    model.fit(model_goods_vectors.toarray(), model_goods_class_values)  # обучаем модель
+    return model, model_vocabular  # возвращаем обученную модель и словарь
 
 
+def good_preparation(path, model_vocabular):
+    good_without_class_description = FileLoader.load_file(path)  # проводим предварительную обработку описания
+    # print(good_without_class_description[0])
+    good_without_class_vector = model_vocabular.transform(
+        good_without_class_description[0])  # переводим описание в векторную форму
+    return good_without_class_vector.toarray()  # возвращаем массиф вектров с описанием товаров
 
+
+def classifay(model, good_without_class_vector):
+    good_class = model.predict(good_without_class_vector)
+    return good_class
+
+
+path = 'teach_file_sample.xlsx'  # обучающий файл
+path2 = 'teach_test_sample.xlsx'  # боевой тестируемый файл
+path3 = 'отладочный файл'  # для тестов и замеров
+
+model = teach_classifier(path)
+goods = good_preparation(path, model[1])
+result = classifay(model[0], goods)
+print(result)
+
+# статистика по тестированию
--- load_file_test.py
+++ load_file_test.py
@@ -1,105 +1,104 @@
 import openpyxl  # импортируем модуль по работе с электронными таблицами
-import time
 import text_transformation
 
-
-"""
-"""
-
-
+#класс загрузки данных из файла для обучения и для тестирования обучения
 class FileLoader:
+    def __init__(self):
+        pass
+    #базовая функция загрузки описаний товаров из файла для обучения и тестирования обучения
+    def load_file(path_to_file):
+        #path_to_file = путь к загружаемому файлу для обучения или тестирования
 
-    def load_file_new(path_to_file, param):
-        start_time = time.time()
 
         file = openpyxl.load_workbook(path_to_file)  # загружам файл в объект для дальнейшей работе с ним
         working_sheet = file.active  # получаю объект лист ддя работы
         goods_description = []  # пустой список под описагния товаров
-        goods_class_values = []  # пустой список под категории для товаров
-        supplier_load_classifir_for_good = []  # пустой список ппод классификатор поставщика
+        goods_target_classes = []  # пустой список sпод категории для товаров
+        goods_supplier_classes = []  # пустой список ппод классификатор поставщика
 
         # чистим выгрузку от лишних ячеек
-        working_sheet = FileLoader.clear_docs_data(working_sheet)
+        working_sheet = FileLoader.clear_file_data(working_sheet)
 
         for row in working_sheet.values:
-            # заполняем таргетинговые значения для обучающей и тестовой выгрузок в зависимости от параметров
-            if param == 1:
-                goods_class_values.append(row[0])
-                start_goods_desc = 1
-            else:
-                start_goods_desc = 0
+            # добавляем категорию товара в коллекцию с таргетинговыми значениями
+            goods_target_classes.append(row[0])
 
             # производим работы с описанием товара и добавляем его в обучающую или тестовую выгрузку
             good = str()
             goods_supplier_class = str()
-            n=0
-            for value in row[start_goods_desc:]:
+            n = 0
+            for value in row[1:]:
+                #получаем путь к категории классификатора поставщика для этого товара
                 if n < 6:
-                    goods_supplier_class = FileLoader.supplier_good_classifier_value(goods_supplier_class, value)
-                n=n+1
-                good = FileLoader.value_enriching(value, row, param, good)
+                    goods_supplier_class = FileLoader.sup_class_replacement(goods_supplier_class, value)
+                n = n + 1
+                #получаем полное описание товара
+                good = FileLoader.text_transformation(value, row, good)
+            #добавляем описание товара в коллекцию описания товаров
             goods_description.append(good)
+            #добавляем описание пути к категории в коллекции пставщика
+            goods_supplier_classes.append(goods_supplier_class)
 
-            # получаем классификатор поставкщика для товара
-            supplier_load_classifir_for_good.append(goods_supplier_class)
+            #print('товар - {} добавлен в выходные списки. \r'.format(len(goods_description)), end="")
 
-        return FileLoader.result_output(param, goods_description, goods_class_values, start_time, file,
-                                        supplier_load_classifir_for_good)
+        file.close()
+        return goods_description, goods_target_classes, goods_supplier_classes
 
-    def value_enriching(value, row, param, good):
-        gender = ['унисекс', 'мужчинам', 'женщинам']
-        vat = {0: 'медицина', 10: 'детский', 20: 'обычный'}
+    #функция обработки слов в описании товара:
+    def text_transformation(value, row, good):
+        #описания справочников:
+        gender = ['любой_пол', 'мужчинам', 'женщинам']
+        vat = {0: 'медицина', 10: 'детский', 20: 'товар_с_обычным_налогом'}
+        chip = {'LOT': 'без_чипа', 'DMX': 'обувь', 'datamatrix': 'обувь', 'MEX': 'мех', 'обычный': 'без_чипа'}
         textile_type = ['текстиль', 'трикотаж', 'другое']
 
+        #удаляем пустые ячейки
         if value == None:
             return good
-        elif value == row[9 - param + 1]:
+        #заменяем числовые значения в описание товара - значениями из справочника
+        elif value == row[9]:
             return good + ' ' + gender[value]
-        elif value == row[11 - param + 1]:
+        elif value == row[11]:
             return good + ' ' + vat[value]
-        elif value == row[14 - param + 1]:
+        elif value == row[13]:
+            return good + ' ' + chip[value]
+        elif value == row[14]:
             return good + ' ' + textile_type[value - 1]
-        #при включенном морфе надо внести в исключение лот и размер
         else:
-            return good+' '+value
-            #return FileLoader.filtr_by_word_form(value,good)
-            #return text_transformation.filtr_by_word_form(value,good)
-    def supplier_good_classifier_value(goods_supplier_class, value):
+        #если никаких изменений не надо - просто добавляем слово в описание товара
+            return good + ' ' + value
+            #приводим значения слов к нормальной форме - пока отключенено
+            # return text_transformation.filtr_by_word_form(value,good)
+
+    #функция замены пустых значений для пути классификатора поставщика на None
+    def sup_class_replacement(goods_supplier_class, value):
         if value != None:
             goods_supplier_class = goods_supplier_class + ' ' + str(value)
         else:
             goods_supplier_class = goods_supplier_class + ' ' + str('None')
         return goods_supplier_class
 
-    def timer(start, param):
-        time_load_for = "Загрузка {0} выборки заняла {1} сек."
-        time_result = time.time() - start
-        if param == "teach":
-            text = str('обучающей')
-        else:
-            text = str('тестовой')
-        time_exect = time_load_for.format(text, time_result)
-        return time_exect
-
-    def clear_docs_data(working_sheet):
+    #функция удаляения пустых и не нужных нам столбцов имеющих шумовой эффект
+    def clear_file_data(working_sheet):
         good_rows = ['Class_ID', 'Уровень 1', 'Уровень 2', 'Уровень 3', 'Уровень 4', 'Уровень 5', 'Уровень 6',
                      'Наименование (<=250)', 'Техническое описание', 'Пол', 'Размер производителя  (<=10)', 'НДС',
                      'Возраст+', 'Тип чипа', 'Тип ткани', 'Тип кроя/джинсы']
         for row in working_sheet:
             for cell in row:
                 if cell.value not in good_rows:
-                    working_sheet.delete_cols(cell.column)
+                    working_sheet.delete_cols(cell.column)  #удаляем столбцы, если их заголовки нам не нужны
             break
         working_sheet.delete_rows(0)
         return working_sheet
 
-    def result_output(param, goods_description, goods_class_values, start_time, file, supplier_load_classifir_for_good):
-        file.close()
-        time_exect = FileLoader.timer(start_time, param)
-        if param == 1:
-            return goods_description, goods_class_values, time_exect, supplier_load_classifir_for_good
-        else:
-            return goods_description, time_exect, supplier_load_classifir_for_good
+
+path = 'teach_file_sample.xlsx'
+path2 = 'teach_test_sample.xlsx'
+
+
+
+# print(FileLoader.load_file(path2,2))
+#print(FileLoader.load_file(path))
 
 
 
@@ -107,4 +106,4 @@ path = 'teach_file_sample.xlsx'
 path2 = 'teach_test_sample.xlsx'
 
 # print(FileLoader.load_file(path2,2))
-print(FileLoader.load_file_new(path, 1))
+# print(FileLoader.load_file_new(path, 1))
